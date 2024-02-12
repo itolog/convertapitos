952ed5ff685183d73601c35f46e033dfc886bc98
Home <>
2024-02-12 21:26:48 +0200

    first commit

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
     tracing_subscriber::fmt().init();
 
     let router = Router::new().path("convert_image").get(convert_image);
-    
+
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
     Server::new(acceptor).serve(router).await;
 }
