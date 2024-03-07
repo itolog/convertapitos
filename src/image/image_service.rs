9a1be27c95e0c6cbbf55f6e80b08fca0fa03c014
itolog <paukanlove@gmail.com>
2024-03-07 20:55:36 +0200

    added simple validation

--- src/image/image_service.rs
+++ src/image/image_service.rs
@@ -1,21 +1,44 @@
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, ImageFormat};
 use salvo::prelude::*;
+use serde::{Deserialize, Serialize};
 use std::io::Cursor;
+use validator::{Validate, ValidationError};
 
 extern crate image;
 
+#[derive(Debug, Validate, Deserialize, Serialize)]
+struct MyData {
+    #[validate(required, custom = "validate_role")]
+    role: Option<String>,
+}
+
+fn validate_role(role: &str) -> Result<(), ValidationError> {
+    if role == "admin" || role == "user" {
+        Ok(())
+    } else {
+        Err(ValidationError::new("terrible_username"))
+    }
+}
 #[handler]
 pub async fn convert_image(req: &mut Request, res: &mut Response) {
-    let file = req.file("file").await;
-    if let Some(file) = file {
-        let input_image: DynamicImage = ImageReader::open(file.path()).unwrap().decode().unwrap();
+    let form = req.parse_form::<MyData>().await.unwrap().validate();
 
-        let mut output_buffer = Vec::new();
-        input_image
-            .write_to(&mut Cursor::new(&mut output_buffer), ImageFormat::Bmp)
-            .expect("Failed to convert image format");
+    match form {
+        Ok(_) => {
+            let file = req.file("file").await;
+            if let Some(file) = file {
+                let input_image: DynamicImage =
+                    ImageReader::open(file.path()).unwrap().decode().unwrap();
 
-        res.write_body(output_buffer).unwrap()
-    }
+                let mut output_buffer = Vec::new();
+                input_image
+                    .write_to(&mut Cursor::new(&mut output_buffer), ImageFormat::Bmp)
+                    .expect("Failed to convert image format");
+
+                res.write_body(output_buffer).unwrap()
+            }
+        }
+        Err(e) => res.status_code(StatusCode::BAD_REQUEST).render(Json(e)),
+    };
 }
