183a122756855dd94a2f715fc8f6f633d4fea6fc
itolog <paukanlove@gmail.com>
2024-04-09 13:05:27 +0300

    resize if ico

--- src/image/image_service.rs
+++ src/image/image_service.rs
@@ -1,8 +1,10 @@
 use crate::image::types::{DataErr, DataResponse, ImageResponse};
 use crate::image::utils::match_ext;
+use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
 use salvo::prelude::*;
+use std::path::Path;
 
 extern crate image;
 
@@ -20,10 +22,18 @@ pub async fn convert_image(req: &mut Request, res: &mut Response) {
         let (file_name, _ext) = file.name().unwrap().split_once('.').unwrap();
         let save_file_path = format!("public/upload/{}.{}", file_name, convert_to.to_lowercase());
         let image_link = format!("upload/{}.{}", file_name, convert_to.to_lowercase());
-        let input_image: DynamicImage = ImageReader::open(file.path()).unwrap().decode().unwrap();
+
+        let mut input_image: DynamicImage = ImageReader::open(Path::new(file.path()))
+            .unwrap()
+            .decode()
+            .unwrap();
+
+        if convert_to == "ico" {
+            input_image = input_image.resize(256, 256, FilterType::Nearest);
+        }
 
         let _ = input_image
-            .save_with_format(save_file_path, match_ext(&convert_to))
+            .save_with_format(Path::new(&save_file_path), match_ext(&convert_to))
             .map(|_| {
                 res.render(Json(ImageResponse {
                     data: Some(DataResponse { image_link }),
