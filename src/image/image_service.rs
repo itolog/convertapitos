1dbbdd14a69a0acfabee94fdca18e1eb618757c3
itolog <paukanlove@gmail.com>
2024-04-10 18:40:01 +0300

    added dynamic folder name

--- src/image/image_service.rs
+++ src/image/image_service.rs
@@ -1,13 +1,17 @@
 use crate::image::types::ImageResponse;
 use crate::image::utils::match_ext;
 use crate::types::{AppResponse, AppResult, DataErr};
+use chrono::{Datelike, Timelike, Utc};
 use image::imageops::FilterType;
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
 use salvo::prelude::*;
 use std::path::Path;
+use tokio::fs::create_dir_all;
 
 extern crate image;
+
+const FOLDER_PATH: &str = "public/uploads";
 
 #[handler]
 pub async fn convert_image(req: &mut Request, res: &mut Response) -> AppResult<()> {
@@ -19,9 +23,32 @@ pub async fn convert_image(req: &mut Request, res: &mut Response) -> AppResult<(
     );
 
     if let Some(file) = image_file {
+        let now = Utc::now();
+        let current_folder_name = format!(
+            "{}-{:02}-{:02}-{:02}",
+            now.year(),
+            now.month(),
+            now.day(),
+            now.minute()
+        );
+        let root_folder = format!("{}/{}", FOLDER_PATH, current_folder_name);
+
+        create_dir_all(Path::new(&root_folder)).await?;
+
         let (file_name, _ext) = file.name().unwrap().split_once('.').unwrap();
-        let save_file_path = format!("public/upload/{}.{}", file_name, convert_to.to_lowercase());
-        let image_link = format!("upload/{}.{}", file_name, convert_to.to_lowercase());
+        let save_file_path = format!(
+            "{}/{}.{}",
+            root_folder,
+            file_name,
+            convert_to.to_lowercase()
+        );
+
+        let image_link = format!(
+            "uploads/{}/{}.{}",
+            current_folder_name,
+            file_name,
+            convert_to.to_lowercase()
+        );
 
         let mut input_image: DynamicImage = ImageReader::open(Path::new(file.path()))?.decode()?;
 
